992e0bb186e679866e515e8cf68ff56eb659ce19
Decodedal <Palumbodallas@gmail.com>
2022-06-16 20:33:54 -0400

    almsot

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+move(character).withArrowKeys(100,250)
+
+
+
+document.addEventListener('keyup', function(e){
+    direction = null
+})
 
 
 move(newImage('assets/tree.png')).to(200, 450)
-move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pillar.png')).withArrowKeys(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
 move(newImage('assets/crate.png')).to(150, 350)
 move(newImage('assets/well.png')).to(500, 575)
